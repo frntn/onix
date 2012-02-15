c44e8b33f42d4c5242b7aa619ca082fefcc111c5
Matthieu Fronton <m@tthieu.fr>
2012-02-15 23:12:17 +0100

    add support for media date

--- lib/onix/media_file.rb
+++ lib/onix/media_file.rb
@@ -11,5 +11,12 @@ module ONIX
     xml_accessor :image_resolution, :from => "ImageResolution"
     xml_accessor :media_file_link_type_code, :from => "MediaFileLinkTypeCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :media_file_link, :from => "MediaFileLink"
+    xml_accessor(:media_file_date, :from => "MediaFileDate", :to_xml => ONIX::Formatters.yyyymmdd) do |val|
+      begin
+        Date.parse(val)
+      rescue
+        nil
+      end
+    end
   end
 end
